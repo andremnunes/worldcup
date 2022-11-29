5742507724f1250770b98639348f07524bc6fedb
andremnunes <119419925+andremnunes@users.noreply.github.com>
2022-11-29 09:59:34 -0300

    Update worldcup_app.py

--- worldcup_app.py
+++ worldcup_app.py
@@ -1,10 +1,8 @@
-from PIL import Image
 import streamlit as st
 import streamlit_book as stb
 
 # Streamlit webpage properties
-im = Image.open("favicon.ico")
-st.set_page_config(page_title="WorldCup", page_icon=im)
+st.set_page_config(page_title="WorldCup")
 
 # Streamlit book properties
 stb.set_book_config(path="pages")
