a161f1e3f544a950c19d80f33876b38ca225671a
andremnunes <119419925+andremnunes@users.noreply.github.com>
2022-11-30 00:58:16 -0300

    Delete 02_Resultado_Partidas.py

--- pages/02_Resultado_Partidas.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import streamlit_book as stb
-from pathlib import Path
-
-current_path = Path(__file__).parent.absolute()
-stb.set_chapter_config(path=current_path  / '03_jogadores.py')
