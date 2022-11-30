ec0dff5addce1c7fe89a3d816ebd65e34a49819b
andremnunes <119419925+andremnunes@users.noreply.github.com>
2022-11-30 01:50:31 -0300

    Update 02.py

--- pages/01_resultado_copa_mundo/02.py
+++ pages/01_resultado_copa_mundo/02.py
@@ -1,4 +1,5 @@
 import streamlit as st
 import streamlit_book as stb
+from pathlib import Path
 
-stb.set_chapter_config(path="01_resultado_copa_mundo")
+stb.set_chapter_config(path= current_path  / "01_resultado_copa_mundo")
