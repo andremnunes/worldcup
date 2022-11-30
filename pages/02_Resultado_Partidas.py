16ccca24ed367ec3c3760738c9634741b4510d70
andremnunes <119419925+andremnunes@users.noreply.github.com>
2022-11-30 00:47:53 -0300

    Update 02_Resultado_Partidas.py

--- pages/02_Resultado_Partidas.py
+++ pages/02_Resultado_Partidas.py
@@ -3,4 +3,4 @@ import streamlit_book as stb
 from pathlib import Path
 
 current_path = Path(__file__).parent.absolute()
-stb.set_chapter_config(path=current_path  / 'pages/03_jogadores.py')
+stb.set_chapter_config(path=current_path  / '03_jogadores.py')
