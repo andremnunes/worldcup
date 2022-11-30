5482b60e96d687d12edbdd6c76028358732624db
andremnunes <119419925+andremnunes@users.noreply.github.com>
2022-11-30 01:54:17 -0300

    Update worldcup.py

--- worldcup.py
+++ worldcup.py
@@ -11,13 +11,22 @@ def main():
     # Streamit book properties
     current_path = Path(__file__).parent.absolute()
     
-    stb.set_book_config(paths="pages",
-                        options=[
-                                      "Resultados das Copas do Mundo",   
-                                      "Resultados das Partidas", 
-                                      "Jogadores", 
-                                      "Clube de Origem", 
-                                      ])
+    stb.set_book_config(menu_title="Datasaurus Rex",
+                    menu_icon="info-square",
+                    options=[
+                            "Welcome!",
+                            "What is a Datasaurus?",
+                            "Where can I see one?",
+                            "Can I create a Datasaurus?",
+                            "About"
+                            ],
+                    paths=[
+                            "docs/01_resultado_copa_mundo",
+                            "docs/01_intro.py",
+                            "docs/03_datasaurus.py",
+                            "docs/04_custom.py",
+                            "docs/05_about.py"
+                          ],
 
 
 if __name__ == "__main__":
