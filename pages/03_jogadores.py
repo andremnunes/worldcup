b927ce214d578f82b5f95fad8cdecf079ad5417e
andremnunes <119419925+andremnunes@users.noreply.github.com>
2022-11-30 00:32:52 -0300

    Update 03_jogadores.py

---
+++ pages/03_jogadores.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import streamlit_book as stb
+from pathlib import Path
+
+current_path = Path(__file__).parent.absolute()
+stb.set_chapter_config(path=current_path  / 'pages/04_clubes_origem.py')
