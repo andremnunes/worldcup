7dd96e484937a39cb2cbdb54eff4415d30b9c8b0
andremnunes <119419925+andremnunes@users.noreply.github.com>
2022-12-01 12:36:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -265,7 +265,7 @@ def set_bg_hack(main_bg):
          f"""
          <style>
          .stApp {{
-             background: url(main_bg);
+             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
              background-size: cover
          }}
          </style>
